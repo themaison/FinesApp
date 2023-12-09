e09e8987ac27440cd348fe1fead82c667a9f0e83
Vladislav Melnichuk <melnichuk1712@mail.ru>
2023-12-09 23:09:49 +0300

    npg added

--- FinesApp/DriverAuthForm.Designer.cs
+++ FinesApp/DriverAuthForm.Designer.cs
@@ -110,8 +110,7 @@
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
             this.button1.Location = new System.Drawing.Point(245, 192);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(280, 60);
